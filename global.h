015eab38bc0d799763be0dd2f4f15083cd1667d1
changguijie <1580507423@qq.com>
2022-08-05 18:31:02 +0800

    epoll

--- global.h
+++ global.h
@@ -17,6 +17,11 @@
 #include <unordered_map>
 #include <pthread.h>
 #include <set>
+
+#include<sys/epoll.h>
+#include<boost/bind.hpp>
+#include<boost/asio.hpp>
+#include<errno.h>
 using namespace std;
 
 #endif
